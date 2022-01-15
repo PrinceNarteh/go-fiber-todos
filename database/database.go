309cd3885ab8b49f629955e61c34f2101cc39382
PrinceNarteh <prinarteh@gmail.com>
2022-01-15 07:13:21 +0000

    add database connection

--- database/database.go
+++ database/database.go
@@ -1,1 +1,22 @@
 package database
+
+import (
+	"go-fiber-todos/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	dsn := "host=localhost user=postgres password=postgres dbname=go_todo port=5432 sslmode=disable TimeZone=Africa/Accra"
+	connection, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Could not connect to database")
+	}
+
+	DB = connection
+
+	connection.AutoMigrate(&models.Todo{})
+}
